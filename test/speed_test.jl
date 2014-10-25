470f84cb2795d57c0439ed864fd844c19f9503ae
Madeleine Udell <madeleine.udell@gmail.com>
2014-10-25 12:53:15 -0700

    import sample in simple_glrms.jl, add speed_test.jl

---
+++ test/speed_test.jl
@@ -0,0 +1,20 @@
+using LowRankModels
+import StatsBase: sample
+
+function fit_pca_nucnorm_sparse_nonuniform(m,n,k,s)
+	# matrix to encode
+	A = randn(m,k)*randn(k,n)
+	losses = fill(quadratic(),n)
+	r = quadreg(.1)
+	obsx = [sample(1:int(m/4),int(s/2)), sample(int(m/4)+1:m,s-int(s/2))] 
+	obsy = sample(1:n,s)
+	obs = [(obsx[i],obsy[i]) for i=1:s]
+	glrm = GLRM(A,obs,losses,r,r,k)
+	X,Y,ch = fit!(glrm)	
+	println("Convergence history:",ch.objective)
+	return A,X,Y,ch
+end
+
+n = 1000
+nobs = 50000
+fit_pca_nucnorm_sparse_nonuniform(n,n,5,nobs)
