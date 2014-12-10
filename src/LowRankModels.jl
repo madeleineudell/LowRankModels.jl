18675f62ab6c6fc597214ad6b50f1bec8247e4a2
Madeleine Udell <madeleine.udell@gmail.com>
2014-12-10 15:07:59 -0500

    use shareglrm whenever nprocs()>1

--- src/LowRankModels.jl
+++ src/LowRankModels.jl
@@ -4,10 +4,12 @@ module LowRankModels
 include("loss_and_reg.jl")
 include("convergence.jl")
 
-## to use many processes to fit a model in shared memory, uncomment line 8 and comment line 9
-#include("shareglrm.jl")
-include("glrm.jl")
-
+## to use many processes to fit a model in shared memory, use shareglrm instead of glrm
+if nprocs()>1
+     include("shareglrm.jl")
+else
+	include("glrm.jl")
+end
 # fancy fun on top of low rank models
 include("cross_validate.jl")
 include("fit_dataframe.jl")
