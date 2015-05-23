540836a4e0cb08bec4d78e0e2525ee4750c6e254
aschuler <aschuler@bmir-macbook-as2.stanford.edu>
2015-05-22 20:28:26 -0700

    renamed

---
+++ src/LowRankModels.jl
@@ -0,0 +1,19 @@
+module LowRankModels
+
+# define losses, regularizers, convergence history
+include("loss_and_reg.jl")
+include("convergence.jl")
+
+## to use many processes to fit a model in shared memory, use shareglrm instead of glrm
+if nprocs()>1
+     include("shareglrm.jl")
+else
+	include("glrm.jl")
+end
+# fancy fun on top of low rank models
+include("initialize.jl")
+include("cross_validate.jl")
+include("fit_dataframe.jl")
+#include("plot.jl")
+
+end # module
