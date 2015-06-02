d9b22e3306d9f0f59851a936f41209a751e03338
Alejandro Schuler <alejandro.schuler@gmail.com>
2015-06-02 09:59:09 -0700

    tested shareglrm and took it out of commission due to weird julia parallelism issues

--- src/LowRankModels.jl
+++ src/LowRankModels.jl
@@ -5,11 +5,12 @@ include("loss_and_reg.jl")
 include("convergence.jl")
 
 # to use many processes to fit a model in shared memory, use shareglrm instead of glrm
-if nprocs()>1
-    include("shareglrm.jl")
-else
-    include("glrm.jl")
-end
+#if nprocs()>1
+#    include("shareglrm.jl")
+#else
+#    include("glrm.jl")
+#end
+include("glrm.jl") # shareglrm is broken, probably because of weird asynchronicity in julia parallelism
 
 # fancy fun on top of low rank models
 include("initialize.jl")
