54d3d0dd877a961599e7e5d4e4fd96c57a038dbd
Alejandro Schuler <alejandro.schuler@gmail.com>
2015-06-26 12:34:46 -0700

    Added files accidentally omitted in earlier commit.

---
+++ src/domains.jl
@@ -0,0 +1,49 @@
+# Supported domains: Real, Boolean, Ordinal, Periodic, Count
+
+# The purpose of domains is to be able to impute over different possible values of `a` regardless of
+# the loss that was used in the GLRM. The reason for doing this is to evaluate the performance of GLRMS.
+# For instance, let's say we use PCA (quadratic losses) to model a binary data frame (not the best idea).
+# In order to override the standard imputation with `impute(quadratic(), u)`, which assumes imputation over the reals,
+# we can use `impute(BoolDomain(), quadratic(), u)` and see which of {-1,1} is best. The reason we want to be able to 
+# do this is to compare a baseline model (e.g. PCA) with a more logical model using heterogenous losses, 
+# yet still give each model the same amount of information regarding how imputation should be done.
+
+# In order to accomplish this we define a series of domains that tell imputation methods 
+# what values the data can take. The imputation methods are defined in impute_and_err.jl
+
+# Domains should be assigned to each column of the data and are not part of the low-rank model itself. 
+# They serve as a way to evaluate the performance of the low-rank model.
+
+export Domain, # the abstract type
+	   RealDomain, BoolDomain, OrdinalDomain, PeriodicDomain, CountDomain # the domains
+
+abstract Domain
+
+########################################## REALS ##########################################
+# Real data can take values from ℜ
+type RealDomain<:Domain
+end
+
+########################################## BOOLS ##########################################
+# Boolean data should take values from {-1,1}
+type BoolDomain<:Domain
+end
+
+########################################## ORDINALS ##########################################
+# Ordinal data should take integer values ranging from `min` to `max`
+type OrdinalDomain<:Domain
+	min::Int
+	max::Int
+end
+
+########################################## PERIODIC ##########################################
+# Periodic data can take values from ℜ, but given a period T, we should have error_metric(a,a+T) = 0
+type PeriodicDomain<:Domain
+	T::Float64 # the period 
+end
+
+########################################## COUNTS ##########################################
+# Count data can take values over ℕ, which we approximate as {0, 1, 2 ... `max_count`}
+type CountDomain<:Domain
+	max_count::Int # the biggest possible count
+end
