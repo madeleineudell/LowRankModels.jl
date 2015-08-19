b031a947980b3eba6b0cf39cd3973b2074ad58d0
Madeleine Udell <madeleine.udell@gmail.com>
2015-08-19 13:58:29 -0700

    added new test for shareGLRMs

---
+++ test/share_test.jl
@@ -0,0 +1,23 @@
+@everywhere using LowRankModels
+using Base.Test
+
+function fit_pca(m,n,k)
+	# matrix to encode
+	srand(1)
+	# generate a matrix with rank k
+	A = randn(m,k)*randn(k,n)
+	# fit a PCA model with rank k
+	glrm = pca(A, k)
+	glrm = share(glrm)
+	p = Params()
+	# just do 10 iterations
+	p.max_iter = 10
+	X,Y,ch = fit!(glrm)
+	return A,X,Y,ch
+end
+
+@everywhere srand(1)
+A,X,Y,ch = fit_pca(100,100,50)	
+
+# make sure objective went down
+@assert ch.objective[end] < ch.objective[1]
