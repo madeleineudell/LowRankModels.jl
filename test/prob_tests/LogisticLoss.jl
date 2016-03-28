289fc3e7e0394f030edf2418542652adba3be000
Madeleine Udell <madeleine.udell@gmail.com>
2016-03-28 15:58:07 -0700

    moved probabilistic loss tests into their own directory and added some more simple ones

---
+++ test/prob_tests/LogisticLoss.jl
@@ -0,0 +1,60 @@
+using LowRankModels
+import StatsBase: sample, WeightVec
+
+# test quadratic loss
+
+## generate data
+srand(1);
+m,n,k = 300,300,3;
+kfit = k+1
+# variance of measurement
+sigmasq = .1
+
+# coordinates of covariates
+X_real = randn(m,k)
+# directions of observations
+Y_real = randn(k,n) 
+
+XY = X_real*Y_real;
+A = zeros(Int, (m, n))
+logistic(x) = 1/(1+exp(-x))
+for i=1:m
+	for j=1:n
+		A[i,j] = logistic(XY[i,j]) >= rand() ? 1 : -1
+	end
+end
+
+# and the model
+losses = LogisticLoss()
+rx, ry = QuadReg(.1), QuadReg(.1);
+glrm = GLRM(A,losses,rx,ry,kfit)
+#scale=false, offset=false, X=randn(kfit,m), Y=randn(kfit,n));
+
+# fit w/o initialization
+p = Params(1, max_iter=10, convergence_tol=0.0000001, min_stepsize=0.000001) 
+@time X,Y,ch = fit!(glrm, params=p);
+XYh = X'*Y;
+println("After fitting, parameters differ from true parameters by $(vecnorm(XY - XYh)/sqrt(prod(size(XY)))) in RMSE")
+A_imputed = impute(glrm)
+println("After fitting, $(sum(A_imputed .!= A) / prod(size(A))*100)\% of imputed entries are wrong")
+println("After fitting, imputed entries are off by $(sum(abs(A_imputed - A)) / prod(size(A))*100)\% on average")
+println("(Picking randomly, 50\% of entries would be wrong.)\n")
+
+# initialize
+init_svd!(glrm)
+XYh = glrm.X' * glrm.Y
+println("After initialization with the svd, parameters differ from true parameters by $(vecnorm(XY - XYh)/sqrt(prod(size(XY)))) in RMSE")
+A_imputed = impute(glrm)
+println("After fitting, $(sum(A_imputed .!= A) / prod(size(A))*100)\% of imputed entries are wrong")
+println("After fitting, imputed entries are off by $(sum(abs(A_imputed - A)) / prod(size(A))*100)\% on average")
+println("(Picking randomly, 50\% of entries would be wrong.)\n")
+
+# fit w/ initialization
+p = Params(1, max_iter=10, convergence_tol=0.0000001, min_stepsize=0.000001) 
+@time X,Y,ch = fit!(glrm, params=p);
+XYh = X'*Y;
+println("After fitting, parameters differ from true parameters by $(vecnorm(XY - XYh)/sqrt(prod(size(XY)))) in RMSE")
+A_imputed = impute(glrm)
+println("After fitting, $(sum(A_imputed .!= A) / prod(size(A))*100)\% of imputed entries are wrong")
+println("After fitting, imputed entries are off by $(sum(abs(A_imputed - A)) / prod(size(A))*100)\% on average")
+println("(Picking randomly, 50\% of entries would be wrong.)\n")
